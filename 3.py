63884330c2f62c2cd39f9c2a3794f08ef8fd508a
TaRa1999R <rezaietara78@gmail.com>
2023-07-30 00:51:04 +0330

    assignment6

--- 3.py
+++ 3.py
@@ -6,19 +6,22 @@ from colorama import Fore
 print (Fore.CYAN , " The shape of equation is : a*x^3 + b*x^2 + c*x + d = 0 " )
 print (" Please enter a , b , c and d as inputs to get the results " , Fore.RESET )
 
-def delta3 ( a , b , c , d ) :
+def delta3 ( a , b , c  ) :
 
-    delta = ( 18*a*b*c*d ) - ( 4*(b**3)*d ) + ( (b**2)*(c**2)) - ( 4*a*(c**3) ) - ( 27*(a**2)*(b**2) )
+    p = b - ( ( a**2 )/3 )
+    q = (( 2*( a**3 ))/27 ) - ( ( a*b )/3 ) + c
+    delta = ( ( q**2 )/4 ) + ( ( p**3 )/ 27 )
+    print (delta, q, p)
     if delta > 0 :
-        print ( Fore.BLUE , " The equation has three real roots " , Fore.RESET )
-        return delta
+        print ( Fore.BLUE , " The equation has one real root " , Fore.RESET )
+        return delta , q , p
     
     elif delta == 0 :
-        print ( Fore.BLUE , " The equation has additional real roots " , Fore.RESET )
+        print ( Fore.BLUE , " The equation has one real root and one additional real roots " , Fore.RESET )
         return delta
     
     else :
-        print (Fore.BLUE , " The equation has a real root and two complex roots " , Fore.RESET )
+        print (Fore.BLUE , " The equation has three roots " , Fore.RESET )
         return delta
     
 def delta2 ( a , b , c ) :
@@ -40,7 +43,6 @@ a = int ( input (" Please enter a : "))
 b = int ( input (" Please enter b : "))
 c = int ( input (" Please enter c : "))
 d = int ( input (" Please enter d : "))
-
 if a == 0 and b != 0 :                                                             #second digree
     print ( Fore.RED , " This is a second degree equation " , Fore.RESET )
     delta = delta2 ( b , c , d )
@@ -70,14 +72,29 @@ elif a == 0 and b == 0 and c == 0 :                                             
 
 else :
     print ( Fore.RED , " This is a third grade equation " , Fore.RESET )
-    delta = delta3 ( a , b , c , d )
+   
+    if a != 1 :
+        b = b / a
+        c = c / a
+        d = d / a
+
+    delta , q , p = delta3 ( b , c , d )
+   
     if delta > 0 :
-        354848
+        x = ((( -q / 2 ) + math.sqrt( delta ))**( 1/3 )) + ((( -q / 2 ) - math.sqrt( delta ))**( 1/3 )) - ( a / 3 )
+        print ( Fore.GREEN , " The answer is : ", x , Fore.RESET )
 
     elif delta == 0 :
-        687956
+        x1 = ( -2 * (( q / 2 )**( 1 / 3 ))) - ( a / 3 )
+        x2 = (( q / 2 )**( 1 / 3 )) - ( a / 3 )
+        print ( Fore.GREEN , " The first answer is : " , x1 , " and the additional answer is : " , x2 , Fore.RESET )
+
 
     elif delta < 0 :
-        684652
+        x1 = (( 2 / ( math.sqrt (3) )) * ( math.sqrt ( -p )) * ( math.sin (( 1 / 3 ) * ( math.asin ( (3 * math.sqrt(3) * q )/( 2 * (( math.sqrt(-p))**3)) ))))) - ( a / 3 )
+        x2 = (( -2 / ( math.sqrt (3))) * ( math.sqrt ( -p )) * ( math.sin ((( 1 / 3 ) * ( math.asin (( 3 * q * ( math.sqrt (3))) / ( 2 * (( math.sqrt (-p))**3 ))))) + ( math.pi / 3 )))) - ( a / 3 )
+        x3 = (( 2 / ( math.sqrt (3) )) * ( math.sqrt ( -p )) * ( math.cos ((( 1 / 3 ) * ( math.asin (( 3 * q * ( math.sqrt (3))) / ( 2 * (( math.sqrt (-p))**3 ))))) + ( math.pi / 6 )))) - ( a / 3 )
+        print ( Fore.GREEN , " The answers are : " , x1 , x2 , x3 , Fore.RESET )
+
 
 print ( Fore.MAGENTA , " DONE " , Fore.RESET)
